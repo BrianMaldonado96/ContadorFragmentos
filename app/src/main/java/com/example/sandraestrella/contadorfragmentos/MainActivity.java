f25211cf2d8f4713dd330c12ab2495a309d5d050
Brian Enrique Maldonado Estrella <14470214@itcampeche.edu.mx>
2017-12-18 22:08:35 -0600

    ContadorFragmento: Aplicación con manejo de eventos

--- app/src/main/java/com/example/sandraestrella/contadorfragmentos/MainActivity.java
+++ app/src/main/java/com/example/sandraestrella/contadorfragmentos/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
         if (fragment == null){
